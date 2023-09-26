16d8fc5974098be3fe87517a5fe87d4613d144a3
NightRoadIx <quintanarxbio@gmail.com>
2023-09-26 16:23:27 -0600

    Update MainActivity.java
    Añadir cuadros de texto y botón para realizar una división, mostrando un mensaje de error

--- MainActivity.java
+++ MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     private Button boton;
     // Para el manejo de una etiqueta de texto
     private TextView vista;
+    private EditText editText1;
+    private EditText editText2;
+    private TextView resultado;
+    private Button boton2;
 
     // MÉTODOS
     @Override
@@ -33,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
         boton = findViewById(R.id.button);
         // Enlazar el cuadro de texto
         vista = findViewById(R.id.textView2);
+        editText1 = findViewById(R.id.editTextText);
+        editText2 = findViewById(R.id.editTextText2);
+        resultado = findViewById(R.id.textView3);
+        boton2 = findViewById(R.id.button2);
+
         // El método recibe como parámetro un objeto OnClickListener
         // (el cual vive en la clase View), lo que genera una subclase
         // anónima
@@ -53,5 +66,48 @@ public class MainActivity extends AppCompatActivity {
                 vista.setText(count.toString());
             }
         });
+
+        boton2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                realizarOperacion("/");
+            }
+        });
+
+
+    }
+    // Método privado, que se usa exclusivamente dentro de la clase
+    private void realizarOperacion(String operador)
+    {
+        // Variables locales
+        double num1 = Double.parseDouble(editText1.getText().toString());
+        double num2 = Double.parseDouble(editText2.getText().toString());
+        double resul = 666;
+
+        try {
+            switch(operador)
+            {
+                case "/":
+                    if(num2 != 0)
+                    {
+                        resul = num1 / num2;
+                    }
+                    else
+                    {
+                        Toast.makeText(this, "División mortal infinita", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+            }
+            //String manda = String.format("%0.2f", resul);
+            NumberFormat nf = NumberFormat.getNumberInstance();
+            nf.setMaximumFractionDigits(2);
+            String rd = nf.format(resul);
+            resultado.setText( rd );
+        }
+        catch(NumberFormatException e)
+        {
+            Toast.makeText(this, "Ingrese números válidos", Toast.LENGTH_SHORT).show();
+        }
     }
 }
