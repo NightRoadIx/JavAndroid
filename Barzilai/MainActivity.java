4a4237dc3308b4f210bd7ebe857a947ecd0a7f61
NightRoadIx <quintanarxbio@gmail.com>
2023-11-29 18:48:05 -0600

    Update MainActivity.java

--- Barzilai/MainActivity.java
+++ Barzilai/MainActivity.java
@@ -23,10 +23,14 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    // #define CONSTANTE 1
+    // Identificar la respuesta de activación Bluetooth
     private static final int REQUEST_ENABLE_BT = 1;
+    // Solicitud de permisos de ubicación
     private static final int REQUEST_LOCATION_PERMISSION = 2;
 
     private BluetoothAdapter bluetoothAdapter;
+    // Objeto<String> forzando a que el objeto sea del tipo String
     private ArrayAdapter<String> deviceListAdapter;
 
     @Override
@@ -34,12 +38,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Definir y conectar a la vez con los widgets
         Button btnScan = findViewById(R.id.button);
         ListView deviceList = findViewById(R.id.deviceList);
 
+        // Adaptador de lista que se utiliza para mostrar los dispositivos disponibles
         deviceListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         deviceList.setAdapter(deviceListAdapter);
 
+        // Utilizar el dispositivo diente azul por defecto
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         if (bluetoothAdapter == null) {
@@ -55,4 +62,80 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void checkBluetoothPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) !=
+                PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADMIN) !=
+                        PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.BLUETOOTH,
+                            Manifest.permission.BLUETOOTH_ADMIN,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            startBluetoothScan();
+        }
+    }
+
+    private void startBluetoothScan() {
+        registerReceiver(bluetoothReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+
+        if(bluetoothAdapter.isDiscovering()) {
+            bluetoothAdapter.cancelDiscovery();
+        }
+        bluetoothAdapter.startDiscovery();
+    }
+
+    private final BroadcastReceiver bluetoothReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName() != null ? device.getName() : "Dispositivo Desconocido";
+                String deviceAddress = device.getAddress();
+                String deviceInfo = deviceName + "\n" + deviceAddress;
+                deviceListAdapter.add(deviceInfo);
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (bluetoothAdapter != null) {
+            bluetoothAdapter.cancelDiscovery();
+        }
+        unregisterReceiver(bluetoothReceiver);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
+                startBluetoothScan();
+            } else {
+                Toast.makeText(this, "Bluetooth no habilitado, la aplicación se cerrará", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startBluetoothScan();
+            } else {
+                Toast.makeText(this, "Permiso de ubicación requerido para escanear dispositivos Bluetooth", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
